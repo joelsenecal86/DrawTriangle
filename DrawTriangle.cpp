8e925c55e5dc97c8fd2f3e7791f5bf24fa7c1473
Joel <joel.senecal@gmail.com>
2021-09-15 21:09:57 -0500

    Initial commit

---
+++ DrawTriangle.cpp
@@ -0,0 +1,37 @@
+// DrawTriangle.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Input Integer: ";
+    int input;
+
+    std::cin >> input;
+
+    for (int i = 0; i <= input; i++)
+    {
+        std::cout << "X";
+
+        for (int j = 0; j < i; j++)
+        {
+           if (j == (i - 1) && i != input)
+            {
+                std::cout << "Y";
+            } 
+
+           if (j != i && i != input)
+           {
+               std::cout << " ";
+           }
+
+           if (i == input)
+           {
+               std::cout << "X";
+           }
+        }
+
+        std::cout << "" << std::endl;        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DrawTriangle
+ 
